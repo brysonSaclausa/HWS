7095144fedc239a5022417a4b4784a0d0a084a8e
brysonS <brys1saclaus2@gmail.com>
2021-09-16 16:47:44 -0400

    Binding state to user interface controls

--- BRSWeSplit/BRSWeSplit/ContentView.swift
+++ BRSWeSplit/BRSWeSplit/ContentView.swift
@@ -10,6 +10,7 @@ import SwiftUI
 struct ContentView: View {
     
     @State private var tapCount = 0
+    @State private var name = ""
     
     var body: some View {
         VStack {
@@ -18,7 +19,13 @@ struct ContentView: View {
             Button("Tap Count \(tapCount)") {
                 self.tapCount += 1
             }
-        }
+            Form {
+                TextField("Enter Your Name", text: $name) //$ introduces two-way binding, we bind the text field so it shows the value of our property 'name', and also writes it back if any changes are made
+                Text("Your name is: \(name)")
+            }
+            
+        }//
+        
     }
 }
 
