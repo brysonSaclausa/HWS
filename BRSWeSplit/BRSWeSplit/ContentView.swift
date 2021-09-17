629370070000dff4b31f2c8bc5e9c58dc847d061
brysonS <brys1saclaus2@gmail.com>
2021-09-16 22:44:22 -0400

    MVP

--- BRSWeSplit/BRSWeSplit/ContentView.swift
+++ BRSWeSplit/BRSWeSplit/ContentView.swift
@@ -9,15 +9,36 @@ import SwiftUI
 
 struct ContentView: View {
     @State private var checkAmount = ""
-    @State private var numberOfPeople = 0
-    @State private var tipPercentage = 2
+    @State private var numberOfPeople = 2
+    @State private var tipPercentage = 0
     
     let tipPercentages = [10, 15, 20, 25, 0]
+    
+    var totalPerPerson: Double {
+        let peopleCount = Double(numberOfPeople + 2)
+        let tipSelection = Double(tipPercentages[tipPercentage])
+        let orderAmount = Double(checkAmount) ?? 0
+        
+        let tipValue = orderAmount / 100 * tipSelection
+        let grandTotal = orderAmount + tipValue
+        let amountPerPerson = grandTotal / peopleCount
+        
+        return amountPerPerson
+    }
+    
+    var totalWithTip: Double {
+        let orderAmount = Double(checkAmount) ?? 0
+        let tipSelection = Double(tipPercentages[tipPercentage])
+        
+        let tipValue = orderAmount / 100 * tipSelection
+        let grandTotal = orderAmount + tipValue
+        
+        return grandTotal
+    }
     
     var body: some View {
         NavigationView {
             Form {
-                
                 Section(header: Text("What is the bill total?")) {
                     TextField("Amount", text: $checkAmount)
                         .keyboardType(.decimalPad)
@@ -31,14 +52,20 @@ struct ContentView: View {
                     }
                 }
                 
-                Section(header: Text("Select percentage")) {
+                Section(header: Text("Select tip percentage"), footer: Text("Total with tip: $\(totalWithTip, specifier: "%.2f")")) {
                     Picker("Tip percentage", selection: $tipPercentage) {
                         ForEach(0 ..< tipPercentages.count) {
                             Text("\(self.tipPercentages[$0])%")
                         }
                         
                     } .pickerStyle(SegmentedPickerStyle())
+                    
                 }
+                
+                Section(header: Text("Amount per person")) {
+                    Text("$\(totalPerPerson, specifier: "%.2f")")
+                }
+                
                 
             } .navigationBarTitle("WeSplit")
         }
