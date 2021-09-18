0bb02df2990d61753d667c287dc8aa67ac49ead4
brysonS <brys1saclaus2@gmail.com>
2021-09-17 23:12:45 -0400

    presenting alert views

--- BRSGuessTheFlag/BRSGuessTheFlag/ContentView.swift
+++ BRSGuessTheFlag/BRSGuessTheFlag/ContentView.swift
@@ -8,36 +8,22 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var showingAlert = false
+    
     var body: some View {
         VStack {
             Button("Tape me!") {
                 print("Button was taped")
             }//basic button
             
-            Button(action: {
-                print("Button was tapped")
-            }) {
-                Text("Tapp me")
-            }//when you want to incorporate images in your buttons
+            Button("Show Alert") {
+                        self.showingAlert = true
+                    }
+                    .alert(isPresented: $showingAlert) {
+                        Alert(title: Text("Hello SwiftUI!"), message: Text("This is some detail message"), dismissButton: .default(Text("OK")))
+                    }
             
-            Button(action: {
-                print("Button was tapped")
-            }) {
-                Image(systemName: "pencil")
-            }//example
             
-            Button(action: {
-                print("Edit button was tapped")
-            }) {
-                HStack(spacing: 10) {
-                    Image(systemName: "pencil").renderingMode(.original)
-                    Text("Edit")
-                }
-                
-            }//example
-            /*
-             Tip: If you find that your images have become filled in with a color, for example showing as solid blue rather than your actual picture, this is probably SwiftUI coloring them to show that they are tappable. To fix the problem, use the renderingMode(.original) modifier to force SwiftUI to show the original image rather than the recolored version.
-             */
 
             
         }
