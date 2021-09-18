558e4d0c493bdf591620c793ef0c98fc56a3c83f
brysonS <brys1saclaus2@gmail.com>
2021-09-17 23:01:42 -0400

    buttons and images

--- BRSGuessTheFlag/BRSGuessTheFlag/ContentView.swift
+++ BRSGuessTheFlag/BRSGuessTheFlag/ContentView.swift
@@ -9,11 +9,34 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        LinearGradient(gradient: Gradient(colors: [.white, .black]), startPoint: .top, endPoint: .bottom)
-        
-        RadialGradient(gradient: Gradient(colors: [.blue, .black]), center: .center, startRadius: 20, endRadius: 200)
-        
-        AngularGradient(gradient: Gradient(colors: [.red, .yellow, .green, .blue, .purple, .red]), center: .center)
+        VStack {
+            Button("Tape me!") {
+                print("Button was taped")
+            }//basic button
+            
+            Button(action: {
+                print("Button was tapped")
+            }) {
+                Text("Tapp me")
+            }//when you want to incorporate images in your buttons
+            
+            Button(action: {
+                print("Button was tapped")
+            }) {
+                Image(systemName: "pencil")
+            }//example
+            
+            Button(action: {
+                print("Edit button was tapped")
+            }) {
+                HStack(spacing: 10) {
+                    Image(systemName: "pencil")
+                    Text("Edit")
+                }
+            }//example
+
+            
+        }
     }
 }
 
