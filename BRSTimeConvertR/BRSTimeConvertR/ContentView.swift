f56523ceebfc37e0bb2fb014c1b551fff1df7477
brysonS <brys1saclaus2@gmail.com>
2021-09-19 00:09:02 -0400

    hours converter - need to work on segmented picker

--- BRSTimeConvertR/BRSTimeConvertR/ContentView.swift
+++ BRSTimeConvertR/BRSTimeConvertR/ContentView.swift
@@ -8,15 +8,54 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var inputHours = ""
+    @State private var inputUnit = 0
+    @State private var unitItem = 0
+    let unitItems = ["hours", "minutes", "seconds"]
+    
+    var hourToMin: Double {
+        let hoursInput = Double(inputHours) ?? 0
+        let outputMinutes = hoursInput * 60
+        return outputMinutes
+    }
+    
+    var hourToSeconds: Double {
+        let hoursInput = Double(inputHours) ?? 0
+        let outputSeconds = hoursInput * 3600
+        return outputSeconds
+    }
+    
+    var hoursToHours: Double {
+        let hoursInput = Double(inputHours) ?? 0
+        return hoursInput
+    }
+    
+    var hoursToDays: Double {
+        let hoursInput = Double(inputHours) ?? 0
+        let converted = hoursInput / 24
+        return converted
+    }
+    
+  
     var body: some View {
-        
         NavigationView {
-            VStack {
+            Form {
                 
-                Form {
-                    Text("Werld")
+                Section {
+                    TextField("Units", text: $inputHours)
+                    Picker("", selection: $unitItem) {
+                        ForEach(0 ..< unitItems.count) {
+                            Text("\(self.unitItems[$0])")
+                        }
+                    } .pickerStyle(SegmentedPickerStyle())
                 }
                 
+                Section {
+                    Text("Seconds: \(hourToSeconds, specifier: "%.2f")")
+                    Text("Minutes: \(hourToMin, specifier: "%.2f")")
+                    Text("Hours: \(hoursToHours, specifier: "%.2f")")
+                    Text("Days: \(hoursToDays, specifier: "%.2f")")
+                }
             }
             .navigationTitle("Time ConvertR")
         }
