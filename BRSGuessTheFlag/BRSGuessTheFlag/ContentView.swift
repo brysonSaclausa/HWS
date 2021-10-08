f81636f484b37e9690fd81c01d3f041544c20603
brysonS <brys1saclaus2@gmail.com>
2021-10-08 18:45:46 -0400

    Stacking up buttons

--- BRSGuessTheFlag/BRSGuessTheFlag/ContentView.swift
+++ BRSGuessTheFlag/BRSGuessTheFlag/ContentView.swift
@@ -9,25 +9,38 @@ import SwiftUI
 
 struct ContentView: View {
     
-    var countries = ["Estonia", "France", "Germany", "Ireland", "Italy", "Nigeria", "Poland", "Russia", "Spain", "UK", "US"]
+    var countries = ["Estonia", "France", "Germany", "Ireland", "Italy", "Nigeria", "Poland", "Russia", "Spain", "UK", "US"].shuffled()
     var correctAnswer = Int.random(in: 0...2)
     
     
     var body: some View {
-        VStack {
-                Text("Tap the flag of")
-                Text(countries[correctAnswer])
-            }
         
-        ForEach(0 ..< 3) { number in
-            Button(action: {
-               // flag was tapped
-            }) {
-                Image(self.countries[number])
-                    .renderingMode(.original)
+        ZStack {
+            Color.blue.edgesIgnoringSafeArea(.all)
+            VStack(spacing: 30) {
+                
+                VStack {
+                    Text("Tap the flag of")
+                    Text(countries[correctAnswer])
+                }
+                ForEach(0 ..< 3) { number in
+                    Button(action: {
+                        // flag was tapped
+                    }) {
+                        Image(self.countries[number])
+                            .renderingMode(.original)
+                    }
+                    Color.blue.edgesIgnoringSafeArea(.all)
+                }
+                
             }
+            
         }
+        
+        
     }
+    
+    
 }
 
 struct ContentView_Previews: PreviewProvider {
